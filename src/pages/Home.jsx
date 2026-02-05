063b3341cb550739a1e646f41fdcac835aa1d9d0
fradevi02 <fra.devito02@gmail.com>
2026-02-05 20:16:54 +0100

    fix: removed theme toggle from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,6 @@ export const Home = () => {
     return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
 
-        {/* Theme Toggle */}
-        <ThemeToggle />
-
         {/* Backgorund Effects */}
         <StarBackground />
 
