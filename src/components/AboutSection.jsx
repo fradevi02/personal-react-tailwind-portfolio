45b7b86b10866e16d5c215cd331efc0f9962f5c9
fradevi02 <fra.devito02@gmail.com>
2026-02-05 22:11:57 +0100

    fix: changed CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a 
-                                href="./public/CV_DeVito.pdf" 
+                                href="/CV_DeVito.pdf" 
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 flex items-center justify-center" 
                                 download
                             >
