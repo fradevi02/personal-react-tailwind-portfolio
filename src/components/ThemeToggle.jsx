c0274f0c916235746c6ee4e9a7dc1cda5e78e69f
fradevi02 <fra.devito02@gmail.com>
2026-02-05 19:58:16 +0100

    fix: changed colour to the moon

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,6 +33,6 @@ export const ThemeToggle = () => {
     return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden right-5 top-5 z-50 p-2 rounded-full transition-colors duration-700", "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary", "bg-gray-400 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600")}> 
         {isDarkMode ? 
             <Sun className="h-6 w-6 text-yellow-300"/> :
-             <Moon className="h-6 w-6 text-blue-900" />} 
+             <Moon className="h-6 w-6 text-blue-600" />} 
         </button>;
 }
